3df47840b44b3249dfd5d80c76085043e8ee8bbd
caleb <caleb@syndicate.io>
2026-01-13 22:29:00 -0500

    fix

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
+	metadataBase: new URL("https://vono.dev"),
 	title: "vono",
 	description: "a template for things",
 	openGraph: {
